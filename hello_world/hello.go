018c2665220d5de1c247e067b3e5fa67ad213926
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-02-05 21:27:07 -0300

    refactor hello world

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -8,8 +8,9 @@ const (
 	englishHelloPrefix = "Hello, "
 	spanishHelloPrefix = "Hola, "
 	frenchHelloPrefix  = "Bonjour, "
-	french             = "French"
-	spanish            = "Spanish"
+
+	french  = "French"
+	spanish = "Spanish"
 )
 
 func Hello(name string, lang string) string {
@@ -17,14 +18,19 @@ func Hello(name string, lang string) string {
 		name = "World"
 	}
 
-	if lang == spanish {
-		return spanishHelloPrefix + name
-	}
+	return greetingPrefix(lang) + name
+}
 
-	if lang == french {
-		return frenchHelloPrefix + name
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return prefix
 }
 
 func main() {
