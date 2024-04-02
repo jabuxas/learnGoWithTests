a007455eb260aca3b149c935f2ffb5b7e7cb8ccc
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-04-02 16:12:29 -0300

    add Delete functionality

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -95,7 +95,12 @@ func TestUpdate(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	word := "test"
-	dictionary := Dictionary{test: "this is just a test"}
+	dictionary := Dictionary{word: "this is just a test"}
 
 	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
 }
