71442dc8be5fa4345e0a39a9b4f1c9dd9dd99890
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-04-03 20:29:42 -0300

    add countdown and learn mocking

---
+++ mocking/countdown.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(w io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(w, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(w, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
